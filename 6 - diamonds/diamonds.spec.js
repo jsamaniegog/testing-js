ee9adb09c60774bed7f1832c8c3e7b16137b93bd
JAVIER SAMANIEGO GARCIA <jsamaniego@saludcastillayleon.es>
2022-06-06 09:57:33 +0200

    Ejercicio 6

--- 6 - diamonds/diamonds.spec.js
+++ 6 - diamonds/diamonds.spec.js
@@ -1,8 +1,23 @@
-async function mine(s) {}
+async function mine(s) {
+    return new Promise((resolve, reject) => {
+        // Llamamos a resolve(...) cuando lo que estabamos haciendo finaliza con éxito, y reject(...) cuando falla.
+        // En este ejemplo, usamos setTimeout(...) para simular código asíncrono con un retardo de 100ms.
+        setTimeout(function () {
+            let diamonds = 0;
 
-test('we can mine diamonds', async () => {
-  await expect(mine('')).resolves.toBe(0);
-  await expect(mine('<>')).resolves.toEqual(1);
-  await expect(mine('<<>>')).resolves.toEqual(2);
-  await expect(mine('<><<>><<')).resolves.toEqual(3);
+            while (s.includes('<>')) {
+                s = s.replace(/[^<>]/g, "");
+                diamonds = s.lenght;
+            }
+
+            resolve(diamonds);
+        }, 100);
+    });
+}
+
+test("we can mine diamonds", async () => {
+    await expect(mine("")).resolves.toBe(0);
+    await expect(mine("<>")).resolves.toEqual(1);
+    await expect(mine("<<>>")).resolves.toEqual(2);
+    await expect(mine("<><<>><<")).resolves.toEqual(3);
 });
