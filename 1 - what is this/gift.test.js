f0058358b8358dfd8989cd92bac1e0dbba099c7e
JAVIER SAMANIEGO GARCIA <jsamaniego@saludcastillayleon.es>
2022-06-06 07:48:23 +0200

    Ejercicio 1

--- 1 - what is this/gift.test.js
+++ 1 - what is this/gift.test.js
@@ -1,5 +1,34 @@
 const gift = require('./gift');
 
-test('what is this?', () => {
-    expect(gift(5)).toBe('');
+test('get gift with a number less than 3', () => {
+    expect(gift(-1)).toBe(null);
+    expect(gift(0)).toBe(null);
+    expect(gift(1)).toBe(null);
+    expect(gift(2)).toBe(null);
+});
+
+test('get gift with five', () => {
+    expect(gift(5)).toBe(
+       "  *\n"
+     + " ***\n"
+     + "*****\n"
+     + " ***\n"
+     + "  *\n"
+   );
+});
+
+test('get gift with six', () => {
+    expect(gift(6)).toBe(null);
+});
+
+test('get gift with seven', () => {
+    expect(gift(7)).toBe(
+       "   *\n"
+     + "  ***\n"
+     + " *****\n"
+     + "*******\n"
+     + " *****\n"
+     + "  ***\n"
+     + "   *\n"
+    );
 });
